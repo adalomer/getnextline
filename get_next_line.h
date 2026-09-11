4659422f35567a0f34bec087e9d81002351919a7
Ömer Ali Adalı <adalomer60@gmail.com>
2026-09-11 16:10:22 +0300

    start find newline function

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: omadali < omadali@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 15:34:32 by omadali           #+#    #+#             */
-/*   Updated: 2026/09/11 15:58:34 by omadali          ###   ########.fr       */
+/*   Updated: 2026/09/11 16:10:22 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,7 @@ typedef struct s_buf
 	char			*content;
 	struct s_buf	*next;
 }	t_buf;
+
+int		find_nl(t_buf *lst);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,29 @@
 /*   By: omadali < omadali@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 15:34:40 by omadali           #+#    #+#             */
-/*   Updated: 2026/09/11 15:34:41 by omadali          ###   ########.fr       */
+/*   Updated: 2026/09/11 16:10:22 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <stdlib.h>
+
+int	find_nl(t_buf *lst)
+{
+	int	i;
+
+	if(!lst)
+		return (0);
+	while (lst)
+	{
+		i = 0;
+		while (lst->content[i])
+		{
+			if (lst->content[i] == '\n')
+				return (1);
+			i++;
+		}
+		lst = lst->next;
+	}
+	return (0);
+}
