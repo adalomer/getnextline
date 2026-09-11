ea0f7db6e5906a9aa5bdf0dff439a72ecdb1fd88
Ömer Ali Adalı <adalomer60@gmail.com>
2026-09-11 17:22:39 +0300

    first try extract line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: omadali < omadali@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 15:34:36 by omadali           #+#    #+#             */
-/*   Updated: 2026/09/11 17:08:17 by omadali          ###   ########.fr       */
+/*   Updated: 2026/09/11 17:22:39 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,4 +31,27 @@ static void	fill_list(int fd, t_buf **lst)
 		buf[bytes] = '\0';
 		append_node(lst, buf);
 	}
+}
+
+static char	*extract_line(t_buf *lst)
+{
+	char	*line;
+	int		i;
+	int		j;
+
+	if (!lst)
+		return (NULL);
+	line = malloc(sizeof(char) * (line_len(lst) + 1));
+	if (!line)
+		return (NULL);
+	j = 0;
+	while (lst)
+	{
+		i = 0;
+		while (lst->content[i] && lst->content[i] != '\n')
+			line[j++] = lst->content[i++];
+		lst = lst->next;
+	}
+	line[j] = '\0';
+	return (line);
 }
