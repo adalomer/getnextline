0728ba9ae5f042a548806a677f76b41f071c506d
Ömer Ali Adalı <adalomer60@gmail.com>
2026-09-11 15:52:08 +0300

    add struct for buffer nodes

--- get_next_line.h
+++ get_next_line.h
@@ -6,11 +6,17 @@
 /*   By: omadali < omadali@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 15:34:32 by omadali           #+#    #+#             */
-/*   Updated: 2026/09/11 15:45:12 by omadali          ###   ########.fr       */
+/*   Updated: 2026/09/11 15:52:08 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+typedef struct s_buf
+{
+	char			*content;
+	struct s_buf	*next;
+}	t_buf;
 
 #endif
