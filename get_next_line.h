4cb4b0c560e125469cdec90d74e3e0e3f9db6de3
Ömer Ali Adalı <adalomer60@gmail.com>
2026-09-11 15:58:34 +0300

    put includes and buffer size

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,19 @@
 /*   By: omadali < omadali@student.42kocaeli.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/11 15:34:32 by omadali           #+#    #+#             */
-/*   Updated: 2026/09/11 15:52:08 by omadali          ###   ########.fr       */
+/*   Updated: 2026/09/11 15:58:34 by omadali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
 typedef struct s_buf
 {
